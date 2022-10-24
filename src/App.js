f5af4cf681c98f96df1425536bfd07446f019639
OlgitH <berman.oj@gmail.com>
2022-10-24 14:13:29 +0100

    some commit message

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Cat from './components/Cat';
+import Dog from './components/Dog';
 
 function App() {
+  const cssStyles={
+    backgroundColor: 'blue'
+  }
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style={cssStyles}>
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Hello this is my first React app
         </p>
+        <Cat />
+        <Dog />
       </header>
     </div>
   );
